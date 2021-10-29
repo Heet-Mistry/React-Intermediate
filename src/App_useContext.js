56ff09ae62a0954f8ffa37c22a9584f7dc6c627d
Heet <heetmistry@gmail.com>
2021-10-29 23:09:06 +0530

    Added the App_useContext component in the index.js file

---
+++ src/App_useContext.js
@@ -0,0 +1,24 @@
+import React ,{useState} from "react";
+import FunctionContextComponent from './FunctionContextComponent'
+import ClassContextComponent from './ClassContextComponent'
+
+export const ThemeContext = React.createContext()
+
+export default function App () {
+
+    const [darkTheme,setDarkTheme] = useState(true)
+
+    function toggleTheme(){
+        setDarkTheme(prevDarkTheme => !prevDarkTheme)
+    }    
+
+    return (
+        <>
+            <ThemeContext.Provider value={darkTheme}>
+                <button onClick={toggleTheme}>Toggle Theme</button>
+                <FunctionContextComponent />
+                <ClassContextComponent />
+            </ThemeContext.Provider>
+        </>
+    )
+}
