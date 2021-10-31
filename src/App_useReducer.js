37370f4591138499eabf69263fa95b69365ac5d1
Heet <heetmistry@gmail.com>
2021-11-01 00:44:02 +0530

    Added the Reducer code for incrementing and decrementing number

---
+++ src/App_useReducer.js
@@ -0,0 +1,39 @@
+import React,{useState,useReducer} from 'react'
+
+function reducer (state , action)
+{
+    switch(action.type){
+
+        case 'increment' : 
+            return {count : state.count +1}
+        
+        case 'decrement' : 
+            return {count : state.count -1}
+
+        default : 
+            return state
+    }
+}
+
+const App_useReducer = () => {
+
+    const [state,dispatch] = useReducer(reducer , { count:0 } ) 
+
+    function increment (){
+        dispatch({type : 'increment'})
+    }
+
+    function decrement (){
+       dispatch({type : 'decrement'})
+    }
+
+    return (
+        <div>
+            <button onClick={decrement}>-</button>
+            <span>{state.count}</span>
+            <button onClick={increment}>+</button>
+        </div>
+    )
+}
+
+export default App_useReducer
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import App2 from './App_useRef';
+import App from './App_useReducer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App2 />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
